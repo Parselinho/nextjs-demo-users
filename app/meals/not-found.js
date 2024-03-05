76eaeff4a5222d2733e4a63ef3211cda31c077ec
Parselinho <parselinho@gmail.com>
2024-03-05 16:52:52 +0200

    first commit

---
+++ app/meals/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <main>
+        <h1>Meal not found</h1>
+        <p>Unfortunately, we could not find the requested page or resource.</p>
+      </main>
+    </>
+  );
+}
---
+++ app/meals/share/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Error() {
+  return (
+    <>
+      <main className="error">
+        <h1>An Error occured!</h1>
+        <p>Failed, please try again later</p>
+      </main>
+    </>
+  );
+}
